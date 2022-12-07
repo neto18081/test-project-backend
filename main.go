27bb456055dcd048f6427f9560e234dc0dd1c505
Pedro <web@agenciaade.com.br>
2022-12-06 21:21:27 -0300

    Making migration automatic

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/neto18081/test-project-backend/controllers"
 	"github.com/neto18081/test-project-backend/initializers"
+	"github.com/neto18081/test-project-backend/models"
 )
 
 func init() {
 	// Load Env Variables and connects to Database
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
+
+	// Makes migrations
+	initializers.DB.AutoMigrate(&models.Customer{})
 }
 
 func main() {
